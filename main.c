f81e16735014175231f866a8fe254c1e85bf5e7a
cmiran <cmiran@student.42.fr>
2019-05-05 19:45:07 +0200

    rev

--- main.c
+++ main.c
@@ -6,17 +6,28 @@
 /*   By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 18:35:41 by cmiran            #+#    #+#             */
-/*   Updated: 2019/04/27 20:14:14 by cmiran           ###   ########.fr       */
+/*   Updated: 2019/05/05 19:12:05 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	parse(int ac, char **av, t_var var)
+{
+	while (ac--)
+	{
+		
+}
+
 int	main(int ac, char **av)
 {
+	t_var	var;
+
+
+
 	if (ac > 1)
 	{
-
+		parse(ac, av);
 	}
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cmiran <cmiran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 19:05:45 by cmiran            #+#    #+#             */
-/*   Updated: 2019/04/27 20:14:33 by cmiran           ###   ########.fr       */
+/*   Updated: 2019/05/05 19:44:59 by cmiran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,16 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef struct	s_list
+{
+	int		val;
+	t_list	*next;
+}	t_list;
+
 typedef struct	s_variables
 {
-	int	*a;
-	int	*b;
+	t_list	*a;
+	t_list	*b;
 }	t_var;
 
 #endif
